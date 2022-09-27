c968c2938873f3509bebe8dcf4b4816a4b9d9041
Olesya <lesusha1@yandex.ru>
2022-09-28 00:03:25 +0300

    add Task_49

---
+++ Task_49/Program.cs
@@ -0,0 +1,51 @@
+﻿// 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[,] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+Console.WriteLine();
+Change2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Print2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write($"{numbers[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Change2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i = i + 2)
+    {
+        for (int j = 0; j < width; j = j + 2)
+        {
+            numbers[i, j] = numbers[i, j] * numbers[i, j];
+        }
+    }
+}
