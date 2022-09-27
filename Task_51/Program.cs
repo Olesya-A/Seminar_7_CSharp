dff841daae1b05076cdccd0f0e7d5a605359ab5b
Olesya <lesusha1@yandex.ru>
2022-09-28 00:16:39 +0300

    add Task_51

---
+++ Task_51/Program.cs
@@ -0,0 +1,52 @@
+﻿// 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[,] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+Console.WriteLine($"Сумма элементов на главной диагонали равна {Changed2DArray(numbers, height, width)}");
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = new Random().Next(0, 11);
+        }
+    }
+}
+
+void Print2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write($"{numbers[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Changed2DArray(int[,] numbers, int height, int width)
+{
+    int sum = 0;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (i == j) sum = sum + numbers[i, j];
+        }
+    }
+    return sum;
+}
