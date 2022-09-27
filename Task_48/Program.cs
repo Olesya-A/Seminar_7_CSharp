5027ba5658d9796cb4ecfb65ada90bd22546cd9f
Olesya <lesusha1@yandex.ru>
2022-09-27 23:52:18 +0300

    add Task_48

---
+++ Task_48/Program.cs
@@ -0,0 +1,44 @@
+﻿// 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[,] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = i + j;
+        }
+    }
+}
+
+
+void Print2DArray(int[,] numbers, int height, int width)
+{    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+           Console.Write($"{numbers[i, j], 3} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
